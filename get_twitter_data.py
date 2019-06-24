43488512e7045a42e07024c26ac2348df253eb65
east5991 <magica.mk.38@gmail.com>
2019-06-24 20:33:19 +0900

    2019/06/24 add

--- get_twitter_data.py
+++ get_twitter_data.py
@@ -14,7 +14,7 @@ from wordcloud import WordCloud
 import collections
 
 #ログ設定
-from logging import getLogger, StreamHandler, DEBUG
+from logging import getLogger, StreamHandler, INFO
 from log_stream import LogStraem
 info_str = '[get_twitter_data info]：'
 
@@ -23,7 +23,7 @@ def analysis_word(tweets_data):
     output = []
     t = Tokenizer('userdic.csv',udic_type="simpledic", udic_enc="utf8")
     for token in t.tokenize(' '.join(tweets_data)):
-        if token.part_of_speech.split(',')[0] in ["名詞"]:
+        if token.part_of_speech.split(',')[0] in ["名詞","カスタム名詞"]:
             output.append(token.surface)
     return output
 
@@ -36,19 +36,18 @@ def analysis_word_rnk(tweets_data):
     for word,count in analyzer.analyze(' '.join(tweets_data)):
             analyze_count += 1
             output.append(word)
-            logger.debug(info_str + '{0}　{1}'.format(word,count))
+            logger.info(info_str + '{0}　{1}'.format(word,count))
             if analyze_count == 10: break
     return output
 
 #wordCloud 出力関数
 def create_wordcloud(text,hashtag,stop_words):
-    fpath = "C:\Windows\Fonts\irohamaru-Regular.ttf"
-    wordCloud = WordCloud(background_color="white",font_path=fpath, width=600, height=500, colormap='spring',stopwords=set(stop_words),max_words=30,max_font_size=300,collocations = False).generate(text)
+    fpath = "C:\Windows\Fonts\YuGothB.ttc"
+    wordCloud = WordCloud(background_color="white",font_path=fpath, width=600, height=500, colormap='spring',stopwords=set(stop_words),max_words=30,max_font_size=300,collocations = False,regexp="[\w']+").generate(text)
     plt.figure(figsize=(6,5))
     plt.imshow(wordCloud)
     plt.axis("off")
-    #plt.show()
-    filename = '{0:%Y-%m-%d_}'.format(datetime.now()) + hashtag + '_figure.png'
+    filename = './picture/{0:%Y-%m-%d_}'.format(datetime.now()) + hashtag + '_figure.png'
     plt.savefig(filename)
     return filename
 
@@ -60,21 +59,19 @@ consumer_key = inifile.get('consumer', 'consumer_key')
 consumer_secret = inifile.get('consumer', 'consumer_secret')
 access_token = inifile.get('access', 'access_token')
 access_secret = inifile.get('access', 'access_secret')
-#ログ
-logger = LogStraem.get_logger()
 # ストップワードの設定
 stop_words = [ u'てる', u'いる', u'なる', u'れる', u'する', u'ある', u'こと', u'これ', u'さん', u'して', \
          u'くれる', u'やる', u'くださる', u'そう', u'せる', u'した',  u'思う',  \
          u'それ', u'ここ', u'ちゃん', u'くん', u'', u'て',u'に',u'を',u'は',u'の', u'が', u'と', u'た', u'し', u'で', \
          u'ない', u'も', u'な', u'い', u'か', u'ので', u'よう', u'', u'なっ',u'ちゃう',u'みよ',u'はず',u'なん',u'でる', \
          u'@',u'#',u'RT',u'w',u'ww',u'www',u'wwww',u'wwwww',u'ｗ',u'ｗｗ',u'ｗｗｗ',u'ｗｗｗｗ',u'ｗｗｗｗｗ',u'anju',u'inami',u':',u'0',u'_',u'agqr',u'https', \
-         u'suwananaka',u'(*',u'*)']
+         u'suwananaka',u'(*',u'*)',u'th',u'なか',u'ー',u'/',u'gt',u't',u'co',u'ん',u'わい',u'5',u'://',u'co',u'(',u'RCC']
 
 #１．引数の判定
 if not len(sys.argv) == 4:
-#if len(sys.argv) == 3:
-    logger.debug(info_str + '引数の設定に誤りがあります。下記のフォーマットで設定して下さい。' + str(len(sys.argv)))
-    logger.debug(info_str + 'python get_twitter_data "hashtag" "開始時間" "終了時間"')
+#if len(sys.argv) == 4:
+    logger.info(info_str + '引数の設定に誤りがあります。下記のフォーマットで設定して下さい。' + str(len(sys.argv)))
+    logger.info(info_str + 'python get_twitter_data "hashtag" "開始時間" "終了時間"')
     sys.exit()
 
 #２．引数の受け取り
@@ -83,29 +80,34 @@ start_time = '{0:%Y-%m-%d_}'.format(datetime.now()) + sys.argv[2] +'_JST'
 end_time = '{0:%Y-%m-%d_}'.format(datetime.now()) + sys.argv[3] +'_jST'
 #テスト用
 #hashtag = 'アッパレ173'
-#start_time = '2019-06-17_' + '23:15:00' +'_JST'
-#end_time = '2019-06-17_' + '23:30:00' +'_JST'
+#start_time = '2019-06-24_' + '23:15:00' +'_JST'
+#end_time = '2019-06-24_' + '23:30:00' +'_JST'
+#hashtag = 'anju_RCC'
+#start_time = '2019-06-20_' + '19:30:00' +'_JST'
+#end_time = '2019-06-20_' + '20:00:00' +'_JST'
 #hashtag = 'fuwasata'
 #start_time = '2019-06-16_' + '21:30:00' +'_JST'
 #end_time = '2019-06-16_' + '22:00:00' +'_JST'
 #ハッシュタグを除外する
 stop_words.append(hashtag)
+#ログ
+logger = LogStraem.get_logger(hashtag)
 
 #３．API認証
-logger.debug(info_str + 'Twitter API認証を開始します。')
+logger.info(info_str + 'Twitter API認証を開始します。')
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth, wait_on_rate_limit = True)
-logger.debug(info_str + 'Twitter API認証が完了しました。')
+logger.info(info_str + 'Twitter API認証が完了しました。')
 
 #４．ツイート取得
-logger.debug(info_str + 'Twitter データ取得を開始します。')
+logger.info(info_str + 'Twitter データ取得を開始します。')
 tweets_data = []
 
 #検索キーワードの設定
 query = '#{0} since:{1} until:{2}'
 query = query.format(hashtag,start_time,end_time)
-logger.debug(info_str + '検索キーワード ' + query)
+logger.info(info_str + '検索キーワード ' + query)
 
 exit_flag = True
 max_id = 0
@@ -120,7 +122,7 @@ while exit_flag:
     else:
         search_results = api.search(q=query,count=100,max_id=max_id)
 
-    logger.debug(info_str + '取得回数 ' + str(loop_count) + '回目　／　' + '取得件数 ' + str(len(search_results)) + '件')
+    logger.info(info_str + '取得回数 ' + str(loop_count) + '回目　／　' + '取得件数 ' + str(len(search_results)) + '件')
     get_data += len(search_results)
     if len(search_results) <= 1: break
 
@@ -130,14 +132,20 @@ while exit_flag:
             tweets_data.append(result.text)
             max_id = result.id
 
-logger.debug(info_str + 'Twitter データ取得が完了しました。')
+logger.info(info_str + 'Twitter データ取得が完了しました。')
+
+#取得件数が0の場合は終了する
+if get_data == 0:
+    logger.info(info_str + 'Twitter データ取得が0件のため、スクリプトを終了します。')
+    sys.exit()
 
 #形態素解析
 analysed_tweets_data = analysis_word(tweets_data)
 
 #ワードクラウド生成・保存
-logger.debug(info_str + 'ワードクラウドの生成を開始します。')
-filename = create_wordcloud(' '.join(analysed_tweets_data),hashtag,stop_words)
+logger.info(info_str + 'ワードクラウドの生成を開始します。')
+text = ' '.join(analysed_tweets_data)
+filename = create_wordcloud(text,hashtag,stop_words)
 filename = os.getcwd() + '/' + filename
 #順位化
 collect = collections.Counter(analysed_tweets_data)
@@ -148,10 +156,9 @@ while len(most_word) == 0:
         most_word = collect.most_common()[word_loop][0]
         break
     word_loop += 1
-logger.debug(info_str + 'ワードクラウドの生成が完了しました。')
+logger.info(info_str + 'ワードクラウドの生成が完了しました。')
 print(collect.most_common(100))
-logger.debug(info_str + filename)
 
 #結果をツイートする
-api.update_with_media(filename = filename,status ='本日最も呟かれたワードは「{0}」でした。  #{1}'.format(most_word,hashtag))
-logger.debug(info_str + 'ツイートが完了しました。')
+api.update_with_media(filename = filename,status ='本日最も呟かれたワードは「{0}」でした。 総ツイート数：{1}件 #{2}'.format(most_word,str(get_data),hashtag))
+logger.info(info_str + 'ツイートが完了しました。')
--- log_stream.py
+++ log_stream.py
@@ -1,12 +1,17 @@
-from logging import getLogger, StreamHandler, DEBUG
+from logging import getLogger, StreamHandler, INFO, FileHandler
+from datetime import datetime
 
 class LogStraem:
     #ログ設定取得
-    def get_logger():
+    def get_logger(hashtag):
         logger = getLogger(__name__)
-        handler = StreamHandler()
-        handler.setLevel(DEBUG)
-        logger.setLevel(DEBUG)
-        logger.addHandler(handler)
+        #ログのファイル出力設定
+        file_handler = FileHandler('./log/' + '{0:%Y-%m-%d_}'.format(datetime.now()) + hashtag + '.log')
+        logger.addHandler(file_handler)
+        #ログのコンソール出力設定
+        stream_handler = StreamHandler()
+        stream_handler.setLevel(INFO)
+        logger.setLevel(INFO)
+        logger.addHandler(stream_handler)
         logger.propagate = False
         return logger
